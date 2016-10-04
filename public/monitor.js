fc6a2752ca0ba3547b7a82b8648344f349ebdb08
U-AKELA\Roxanne <roxanne.coburn@gmail.com>
2016-10-04 17:24:27 -0400

    added object arrays

--- public/monitor.js
+++ public/monitor.js
@@ -1,9 +1,12 @@
 var socket = io('/terie');     //http://socket.io/docs/
 var message_state = 0;
+var buttons = [];
+var y_thing = 100;
 
 function setup(){
   createCanvas(400, 400);
   background(0);
+     
 }
 
 function preload() {
@@ -15,44 +18,81 @@ function preload() {
     sad = loadImage("images/sad.png");
 }
 
-function draw(){
 
+
+function draw(){
+    if(frameCount > 400) {
+        frameCount = frameCount - Math.floor(frameCount / 400) * 400;
+    }
+   
     background(255,255,255,255);
     
     fill(255, 102, 153);
 
     //image(wow, -frameCount+400, 100, 100, 100);
     textSize(20);
-
+    
   //a very simple state machine
     if(message_state==0){
-        text("Nothing to See Here", 100, 100);
+        //text("Nothing to See Here", 100, 100);
     }
     else if(message_state==1){
         //text("like", 100, 100);
-        image(like, -frameCount+400, 100, 100, 100);
+        add_button(like);
+        
+        //image(like, -frameCount+400, 100, 100, 100);
     }
     else if(message_state==2){
         //text("love", 100, 100);
-        image(like, -frameCount+400, 100, 100, 100);
+        add_button(love);
+        //image(like, -frameCount+400, 100, 100, 100);
     }
     else if(message_state==3){
        // text("haha", 100, 100);
-        image(haha, -frameCount+400, 100, 100, 100);
+        add_button(haha);
+        //image(haha, -frameCount+400, 100, 100, 100);
     }
     else if(message_state==4){
        // text("wow", 100, 100);
-        image(wow, -frameCount+400, 100, 100, 100);
+        add_button(wow);
+        //image(wow, -frameCount+400, 100, 100, 100);
     }
     else if(message_state==5){
         //text("angry", 100, 100);
-        image(angry, -frameCount+400, 100, 100, 100);
+        add_button(angry);
+        //image(angry, -frameCount+400, 100, 100, 100);
     }
    else if(message_state==6){
        //text("sad", 100, 100);
-       image(sad, -frameCount+400, 100, 100, 100);
+       add_button(sad);
+       //image(sad, -frameCount+400, 100, 100, 100);
   }
+    for(var i = 0; i < buttons.length; i++) {
+        buttons[i].display();
+        buttons[i].update();
+    }
 }
+
+
+function add_button(type) {
+    var newButton = {
+        type: type,
+        //final_x: random(0,400),
+        //final_y: random(0,400),
+        init_x: 400,
+        init_y: 100,
+        display: function() {
+            image(type, this.init_x, this.init_y, 100, 100);
+        },
+        update: function() {
+            this.init_x = 400-frameCount ;
+        }
+    };
+    buttons.push(newButton);
+        
+}
+
+
 
 socket.on('like', function(){
   console.log("like fired");
@@ -83,3 +123,4 @@ socket.on('sad', function(){
   console.log("sad");
   message_state=6;
 })
+
